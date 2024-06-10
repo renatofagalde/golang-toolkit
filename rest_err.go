f532751937716f06d0b7aceb6575541149b14bf3
renato@likwi.com.br <renatofagalde>
2024-06-10 13:03:12 -0700

    rest_err.go

---
+++ rest_err.go
@@ -0,0 +1,83 @@
+package toolkit
+
+import "net/http"
+
+type RestErr struct {
+	Message string  `json:"message"`
+	Err     string  `json:"error"`
+	Code    int     `json:"code"`
+	Causes  []Cause `json:"causes"`
+}
+
+type Cause struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
+
+// construtor
+func NewRestErr(message, err string, code int, causes []Cause) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     err,
+		Code:    code,
+		Causes:  causes,
+	}
+}
+
+func (r *RestErr) Error() string {
+	return r.Message
+}
+
+// bad request
+func NewBadRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+	}
+}
+
+// badrequest validando objeto
+func NewBadRequestValidationError(message string, causes []Cause) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+		Causes:  causes,
+	}
+}
+
+// erro interno
+func NewInternalServerError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "internal_server_error",
+		Code:    http.StatusInternalServerError,
+	}
+}
+
+// forbidden
+func NewForbiddenError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "forbidden",
+		Code:    http.StatusForbidden,
+	}
+}
+
+// notfound error
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "notFound",
+		Code:    http.StatusNotFound,
+	}
+}
+
+func NewUnauthorizedRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "unauthorizes",
+		Code:    http.StatusUnauthorized,
+	}
+}
