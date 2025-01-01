65aafe5c20f11a372c683bfc1268582a02642e9b
Renato Fagalde <renato@likwi.com.br>
2025-01-01 17:24:09 -0300

    validacao tempo do token

--- tools.go
+++ tools.go
@@ -275,6 +275,35 @@ func (t *Tools) LoadConfigFile(path string) (config Config, err error) {
 	return
 }
 
+func (t *Tools) LoadConfigEnv() (config Config, err error) {
+
+	config.DBDriver = os.Getenv("DB_DRIVER")
+	config.DBSource = os.Getenv("DB_SOURCE")
+	config.ServerAddress = os.Getenv("SERVER_ADDRESS")
+	config.TokenSymmetricKey = os.Getenv("TOKEN_SYMMETRIC_KEY")
+
+	accessTokenDurationStr := os.Getenv("ACCESS_TOKEN_DURATION")
+	if accessTokenDurationStr == "" {
+		err = fmt.Errorf("variável de ambiente ACCESS_TOKEN_DURATION não está definida ou está vazia")
+		return
+	}
+
+	config.AccessTokenDuration, err = time.ParseDuration(accessTokenDurationStr)
+	if err != nil {
+		err = fmt.Errorf("erro ao converter ACCESS_TOKEN_DURATION para time.Duration: %w", err)
+		return
+	}
+
+	if config.DBDriver == "" || config.ServerAddress == "" || config.TokenSymmetricKey == "" {
+		err = fmt.Errorf("algumas variáveis obrigatórias não estão definidas no ambiente")
+		return
+	}
+
+	config.DBSource = t.buildDBSource(config.DBSource)
+
+	return
+}
+
 func (t *Tools) buildDBSource(defaultDBSource string) string {
 	dbUser := os.Getenv("app_database_user")
 	dbPassword := os.Getenv("app_database_password")
