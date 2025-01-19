66ed805f091c4d1c7632b843ad89cedab113d3a1
Renato Fagalde <renato@likwi.com.br>
2025-01-19 18:23:31 -0300

    :sparkles: feat: hashpasword

--- password.go
+++ password.go
@@ -1,17 +1,20 @@
 package toolkit
 
 import (
-	"fmt"
 	"golang.org/x/crypto/bcrypt"
 )
 
 type Password struct {
 }
 
-func (t *Password) HashPassword(password string) (string, error) {
+func (t *Password) HashPassword(password string) (string, *RestErr) {
+
 	hashPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	//if err != nil {
+	//	return "", fmt.Errorf("failed to has password: %w", err)
+	//}
 	if err != nil {
-		return "", fmt.Errorf("failed to has password: %w", err)
+		return "", (&RestErr{}).NewBadRequestError("failed to hash password")
 	}
 
 	return string(hashPassword), nil
