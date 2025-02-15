8cff83df7c048b549381cc9001f814a2c5467fd9
Renato Fagalde <renato@likwi.com.br>
2025-02-15 19:27:16 -0300

    :sparkles: feat: password interface

--- password.go
+++ password.go
@@ -4,15 +4,21 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+type PasswordCrypt interface {
+	HashPassword(password string) (string, *RestErr)
+	CheckPassword(hashedPassword string, password string) error
+}
+
 type Password struct {
+}
+
+func NewPassword() PasswordCrypt {
+	return &Password{}
 }
 
 func (t *Password) HashPassword(password string) (string, *RestErr) {
 
 	hashPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	//if err != nil {
-	//	return "", fmt.Errorf("failed to has password: %w", err)
-	//}
 	if err != nil {
 		return "", (&RestErr{}).NewBadRequestError("failed to hash password")
 	}
